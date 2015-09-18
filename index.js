302d311c27d5e130c269196923970e5bdc4a1025
Aaron Larner <anlarner@gmail.com>
2015-09-17 21:50:50 -0500

    Fix bug when there are no children

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 var React = require('react');
 module.exports = React.createClass({
 	render: function() {
+
+		var notFound = this.props.notFound || <div>page not found</div>;
+
+		if(!this.props.children) {
+			return notFound;
+		}
+
 		for(var i=0; i < this.props.children.length; i++) {
 			if(this.props.children[i].props.page === this.props.page) {
 				return this.props.children[i];
 			}
 		}
 
-		return this.props.notFound || <div>page not found</div>;
+		return notFound;
 	}
 });
